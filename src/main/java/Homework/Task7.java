67dd52530f9a836b0a95edf83702d2bbebf160a6
Luda <millya92@gmail.com>
2022-12-09 00:00:05 +0300

    seven

---
+++ src/main/java/Homework/Task7.java
@@ -0,0 +1,19 @@
+/** Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100.
+ */
+package Homework;
+import java.util.Arrays;
+public class Task7 {
+    public static void main(String[] args) {
+        ex1();
+    }
+
+    static void ex1() {
+        int[] FillArray = new int[100];
+        for (int i = 0; i < FillArray.length; i++) {
+            FillArray[i] = i + 1;
+        }
+        System.out.println("массив: ");
+        System.out.println(Arrays.toString(FillArray));
+    }
+}
+
